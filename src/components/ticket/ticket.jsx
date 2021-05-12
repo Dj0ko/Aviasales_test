c53db9cff3d1b0f77e8ce676b9111120b3b7f1f6
Anton <antonr1@bk.ru>
2021-05-12 17:17:07 +0300

    fix ticket & tickets

---
+++ src/components/ticket/ticket.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './ticket.module.scss';
+
+const Ticket = ({ ticket }) => {
+  // Функция для добавления 0 перед значениями состоящими из одной цифры
+  const addZero = (data) => {
+    if (String(data).length === 1) {
+      return `0${data}`;
+    }
+    return data;
+  };
+
+  // Функция для получения количества часов и минут
+  const getTime = (value) => {
+    const hours = Math.floor(value / 60);
+    const minutes = value % 60;
+
+    return {
+      hours,
+      minutes,
+    };
+  };
+
+  // Функция для расстановки правильных окончаний в зависимости от количества пересадок
+  const addStops = (data) => {
+    switch (data.length) {
+      case 0:
+        return 'Без пересадок';
+
+      case 1:
+        return '1 пересадка';
+
+      default:
+        return `${data.length} пересадки`;
+    }
+  };
+
+  // Получаем цену билета
+  const ticketPrice = `${String(ticket.price).substr(0, 2)} ${String(ticket.price).substr(2)} Р`;
+  // Получаем логотип авиакомпании
+  const ticketAviaLogo = `//pics.avs.io/99/36/${ticket.carrier}.png`;
+  // Получаем данные о билете "Туда"
+  const oneWayData = ticket.segments[0];
+  // Получаем данные о времени билета "Туда"
+  const oneWayDate = new Date(oneWayData.date);
+  // Получаем продолжительность полёта в минутах и количество пересадок
+  const { duration, stops } = oneWayData;
+  // Высчитываем время прилёта
+  const oneWayDestinationTime = new Date(Date.parse(oneWayDate) + duration * 60000);
+
+  // Получаем данные о билете "Обратно"
+  const backWayData = ticket.segments[1];
+  // Получаем данные о времени билета "Обратно"
+  const backWayDate = new Date(backWayData.date);
+  // Получаем продолжительность полёта в минутах и количество пересадок в обратную сторону
+  const { duration: durationBack, stops: stopsBack } = backWayData;
+  // Высчитываем время прилёта
+  const backWayDestinationTime = new Date(Date.parse(backWayDate) + durationBack * 60000);
+
+  return (
+    <li
+      className={classes.ticket}
+      key={`${oneWayData.origin + oneWayData.destination + ticketPrice + stops.join() + stopsBack.join()}`}
+    >
+      <div className={classes.ticket__container}>
+        <span className={classes.ticket__price}>{ticketPrice}</span>
+        <img src={ticketAviaLogo} alt="логотип авиакомпании" />
+      </div>
+      <dl className={`${classes.ticket__route} ${classes['ticket__route--one-way']}`}>
+        <div>
+          <dt>
+            {oneWayData.origin} - {oneWayData.destination}
+          </dt>
+          <dd>
+            {`${addZero(oneWayDate.getUTCHours())}:${addZero(oneWayDate.getUTCMinutes())} - 
+          ${addZero(oneWayDestinationTime.getUTCHours())}:${addZero(oneWayDestinationTime.getUTCMinutes())}`}
+          </dd>
+        </div>
+        <div>
+          <dt>В пути</dt>
+          <dd>
+            {getTime(duration).hours}ч {getTime(duration).minutes}м
+          </dd>
+        </div>
+        <div>
+          <dt>{addStops(stops)}</dt>
+          <dd>{stops.length !== 0 ? stops.join(', ') : '-'}</dd>
+        </div>
+      </dl>
+      <dl className={classes.ticket__route}>
+        <div>
+          <dt>
+            {backWayData.origin} - {backWayData.destination}
+          </dt>
+          <dd>
+            {`${addZero(backWayDate.getUTCHours())}:${addZero(backWayDate.getUTCMinutes())} - 
+          ${addZero(backWayDestinationTime.getUTCHours())}:${addZero(backWayDestinationTime.getUTCMinutes())}`}
+          </dd>
+        </div>
+        <div>
+          <dt>В пути</dt>
+          <dd>
+            {getTime(durationBack).hours}ч {getTime(durationBack).minutes}м
+          </dd>
+        </div>
+        <div>
+          <dt>{addStops(stopsBack)}</dt>
+          <dd>{stopsBack.length !== 0 ? stopsBack.join(', ') : '-'}</dd>
+        </div>
+      </dl>
+    </li>
+  );
+};
+
+export default Ticket;
+
+Ticket.defaultProps = {
+  ticket: {},
+};
+
+Ticket.propTypes = {
+  ticket: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.arrayOf(PropTypes.objectOf)])
+  ),
+};
--- src/components/tickets/tickets.jsx
+++ src/components/tickets/tickets.jsx
@@ -3,137 +3,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../spinner/spinner';
 import Sorting from '../sorting/sorting';
+import Ticket from '../ticket/ticket';
 
 import * as actions from '../../redux/actions/actions';
 
 import classes from './tickets.module.scss';
 
 const Tickets = ({ ticketsList, loadingState }) => {
-  // Функция для добавления 0 перед значениями состоящими из одной цифры
-  const addZero = (data) => {
-    if (String(data).length === 1) {
-      return `0${data}`;
-    }
-    return data;
-  };
-
-  // Функция для получения количества часов и минут
-  const getTime = (value) => {
-    const hours = Math.floor(value / 60);
-    const minutes = value % 60;
-
-    return {
-      hours,
-      minutes,
-    };
-  };
-
-  // Функция для расстановки правильных окончаний в зависимости от количества пересадок
-  const addStops = (data) => {
-    switch (data.length) {
-      case 0:
-        return 'Без пересадок';
-
-      case 1:
-        return '1 пересадка';
-
-      default:
-        return `${data.length} пересадки`;
-    }
-  };
-
   if (ticketsList[0] === 'nothing') {
     return <p className={classes.tickets__info}>Рейсов, подходящих под заданные фильтры, не найдено</p>;
   }
+
+  const allTickets = ticketsList.map((ticket, index) => {
+    if (index < 5) {
+      return <Ticket ticket={ticket} key={`${ticket.carrier + ticket.price + ticket.segments[0].duration}`} />;
+    }
+    return '';
+  });
 
   return (
     <section className={classes.tickets}>
       <Sorting />
       {loadingState && <Spinner />}
-      <ul>
-        {ticketsList.map((ticket, index) => {
-          // Получаем цену билета
-          const ticketPrice = `${String(ticket.price).substr(0, 2)} ${String(ticket.price).substr(2)} Р`;
-          // Получаем логотип авиакомпании
-          const ticketAviaLogo = `//pics.avs.io/99/36/${ticket.carrier}.png`;
-          // Получаем данные о билете "Туда"
-          const oneWayData = ticket.segments[0];
-          // Получаем данные о времени билета "Туда"
-          const oneWayDate = new Date(oneWayData.date);
-          // Получаем продолжительность полёта в минутах и количество пересадок
-          const { duration, stops } = oneWayData;
-          // Высчитываем время прилёта
-          const oneWayDestinationTime = new Date(Date.parse(oneWayDate) + duration * 60000);
-
-          // Получаем данные о билете "Обратно"
-          const backWayData = ticket.segments[1];
-          // Получаем данные о времени билета "Обратно"
-          const backWayDate = new Date(backWayData.date);
-          // Получаем продолжительность полёта в минутах и количество пересадок в обратную сторону
-          const { duration: durationBack, stops: stopsBack } = backWayData;
-          // Высчитываем время прилёта
-          const backWayDestinationTime = new Date(Date.parse(backWayDate) + durationBack * 60000);
-
-          if (index < 5) {
-            return (
-              <li
-                className={`${classes.tickets__ticket} ${classes.ticket}`}
-                key={`${oneWayData.origin + oneWayData.destination + ticketPrice + stops.join() + stopsBack.join()}`}
-              >
-                <div className={classes.ticket__container}>
-                  <span className={classes.ticket__price}>{ticketPrice}</span>
-                  <img src={ticketAviaLogo} alt="логотип авиакомпании" />
-                </div>
-                <dl className={`${classes.ticket__route} ${classes['ticket__route--one-way']}`}>
-                  <div>
-                    <dt>
-                      {oneWayData.origin} - {oneWayData.destination}
-                    </dt>
-                    <dd>
-                      {`${addZero(oneWayDate.getUTCHours())}:${addZero(oneWayDate.getUTCMinutes())} - 
-                    ${addZero(oneWayDestinationTime.getUTCHours())}:${addZero(oneWayDestinationTime.getUTCMinutes())}`}
-                    </dd>
-                  </div>
-                  <div>
-                    <dt>В пути</dt>
-                    <dd>
-                      {getTime(duration).hours}ч {getTime(duration).minutes}м
-                    </dd>
-                  </div>
-                  <div>
-                    <dt>{addStops(stops)}</dt>
-                    <dd>{stops.length !== 0 ? stops.join(', ') : '-'}</dd>
-                  </div>
-                </dl>
-                <dl className={classes.ticket__route}>
-                  <div>
-                    <dt>
-                      {backWayData.origin} - {backWayData.destination}
-                    </dt>
-                    <dd>
-                      {`${addZero(backWayDate.getUTCHours())}:${addZero(backWayDate.getUTCMinutes())} - 
-                    ${addZero(backWayDestinationTime.getUTCHours())}:${addZero(
-                        backWayDestinationTime.getUTCMinutes()
-                      )}`}
-                    </dd>
-                  </div>
-                  <div>
-                    <dt>В пути</dt>
-                    <dd>
-                      {getTime(durationBack).hours}ч {getTime(durationBack).minutes}м
-                    </dd>
-                  </div>
-                  <div>
-                    <dt>{addStops(stopsBack)}</dt>
-                    <dd>{stopsBack.length !== 0 ? stopsBack.join(', ') : '-'}</dd>
-                  </div>
-                </dl>
-              </li>
-            );
-          }
-          return '';
-        })}
-      </ul>
+      <ul>{allTickets}</ul>
     </section>
   );
 };
