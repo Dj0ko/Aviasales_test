d9ebfa48407d2db0c542a85da51a8603d0d11607
Anton <antonr1@bk.ru>
2021-05-26 14:37:53 +0300

    fix filter reducer

--- src/redux/reducers/filter-reducer.js
+++ src/redux/reducers/filter-reducer.js
@@ -1,16 +1,11 @@
 const filterReducer = (state = [true, true, true, true, true], action) => {
   switch (action.type) {
     case 'ALL_TRANSFERS':
-      if (state[0] || (!state[0] && state[1] && state[2] && state[3] && state[4])) {
-        return [false, false, false, false, false];
-      }
-      return [true, true, true, true, true];
+      return (state[0] || (!state[0] && state[1] && state[2] && state[3] && state[4])) ? [false, false, false, false, false] : [true, true, true, true, true];
 
     case 'CHANGE_FILTER':
-      if (state[action.payload]) {
-        return [false, ...state.slice(1, action.payload), !state[action.payload], ...state.slice(action.payload + 1)];
-      }
-      return [...state.slice(0, action.payload), !state[action.payload], ...state.slice(action.payload + 1)];
+      return (state[action.payload]) ? [false, ...state.slice(1, action.payload), !state[action.payload], ...state.slice(action.payload + 1)] : 
+      [...state.slice(0, action.payload), !state[action.payload], ...state.slice(action.payload + 1)]
       
     
     default:
