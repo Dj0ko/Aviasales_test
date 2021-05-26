2ac0211783e502077974afe9b8497976804f6000
Anton <antonr1@bk.ru>
2021-05-26 16:31:31 +0300

    fix filter-reducer element===true

--- src/redux/reducers/filter-reducer.js
+++ src/redux/reducers/filter-reducer.js
@@ -3,7 +3,7 @@ const filterReducer = (state = Array(5).fill(true), action) => {
 
   switch (type) {
     case 'ALL_TRANSFERS':
-      return (state[0] || (!state[0] && state.slice(1).every(element => element === true))) ? Array(5).fill(false) : Array(5).fill(true);
+      return (state[0] || (!state[0] && state.slice(1).every(element => element))) ? Array(5).fill(false) : Array(5).fill(true);
 
     case 'CHANGE_FILTER':
       return (state[payload]) ? [false, ...state.slice(1, payload), !state[payload], ...state.slice(payload + 1)] : 
